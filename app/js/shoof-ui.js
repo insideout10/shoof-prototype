974d671477b93de43158c5f9bd1e9aba820e42eb
Marcello Colacino <marcello.colacino@gmail.com>
2014-09-20 05:08:34 +0200

    First basic release

--- app/js/shoof-ui.js
+++ app/js/shoof-ui.js
@@ -1,39 +1,129 @@
 (function() {
-  var app;
+  var app,
+    __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
 
   app = angular.module("shoof.ui", []);
 
   app.controller("ShoofCtrl", [
-    "ContextManagerService", "DataRetrieverService", "$scope", "$log", function(ContextManagerService, DataRetrieverService, $scope, $log) {
-      return $scope.dataFor = function(containerOrigin) {
-        return DataRetrieverService.retrieveOrLoadDataStructureFor(ctn);
+    "ContextManagerService", "DataRetrieverService", "$scope", "$rootScope", "$log", function(ContextManagerService, DataRetrieverService, $scope, $rootScope, $log) {
+      var contextChanged;
+      $scope.stack = {};
+      $scope.context = ContextManagerService.getContext();
+      contextChanged = function(property, value) {};
+      $rootScope.$on("contextChanged", function(event, property, value) {
+        if (ContextManagerService.addProperty(property, value)) {
+          return ContextManagerService.rewriteStack($scope.stack);
+        }
+      });
+      $rootScope.$on("containerLoaded", function(event, ctnOrigin) {
+        $log.debug("Notified about ctn " + ctnOrigin + " loading");
+        $scope.stack[ctnOrigin] = ctnOrigin;
+        return $scope.$digest();
+      });
+      $scope.dataFor = function(ctnOrigin) {
+        return DataRetrieverService.retrieveOrLoadDataStructureFor(ctnOrigin);
+      };
+      $scope.test = function() {
+        $log.debug("Rewriting ctn uri");
+        return $scope.stack["/data/News1.json"] = "/data/News1---foo=a.json";
+      };
+      $scope.submit = function() {
+        $log.debug("submit");
+        return $rootScope.$broadcast("contextChanged", $scope.contextProperty, $scope.contextPropertyValue);
+      };
+      return $scope.reset = function() {
+        $log.debug("reset");
+        ContextManagerService.resetContext;
+        ContextManagerService.resetStack($scope.stack);
+        $scope.contextProperty = void 0;
+        return $scope.contextPropertyValue = void 0;
       };
     }
   ]);
 
   app.service("ContextManagerService", [
-    function() {
+    "$log", function($log) {
       var service;
-      service = {};
-      service.foo = function() {
-        console.log("I am the context");
+      service = {
+        _context: {},
+        _allowedProperties: ['foo']
+      };
+      service.getContext = function() {
+        return this._context;
+      };
+      service.addProperty = function(property, value) {
+        if (__indexOf.call(this._allowedProperties, property) < 0) {
+          $log.warn("ContextManager does not allow property " + property);
+          return false;
+        }
+        this._context[property] = value;
+        return true;
+      };
+      service.toString = function() {
+        var out, property, _i, _len, _ref;
+        out = "---";
+        _ref = this._allowedProperties;
+        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+          property = _ref[_i];
+          out += "" + property + "=" + this._context[property];
+        }
+        out += ".json";
+        return out;
+      };
+      service.rewriteOrigin = function(origin) {
+        return origin.replace(".json", this.toString());
+      };
+      service.rewriteStack = function(stack) {
+        var id, origin;
+        for (id in stack) {
+          origin = stack[id];
+          stack[id] = this.rewriteOrigin(id);
+        }
+        return stack;
+      };
+      service.resetStack = function(stack) {
+        var id, origin;
+        for (id in stack) {
+          origin = stack[id];
+          stack[id] = id;
+        }
+        return stack;
+      };
+      service.resetContext = function() {
+        return this._context = {};
       };
       return service;
     }
   ]);
 
   app.service("DataRetrieverService", [
-    "$log", function($log) {
+    "$http", "$log", "$rootScope", function($http, $log, $rootScope) {
       var service;
       service = {
         _containers: {}
       };
       service.setInPageContainers = function(containers) {
-        $log.debug("Going to set containers " + containers);
-        return this._containers = containers;
+        var origin, _results;
+        this._containers = containers;
+        _results = [];
+        for (origin in this._containers) {
+          $log.debug("Going to notify ctn " + origin + " was loaded!");
+          _results.push($rootScope.$broadcast("containerLoaded", origin));
+        }
+        return _results;
       };
-      service.retrieveOrLoadDataStructureFor = function(containerOrigin) {
-        return this._containers[containerOrigin];
+      service.retrieveOrLoadDataStructureFor = function(ctnOrigin) {
+        var container, promise;
+        if (!ctnOrigin) {
+          return;
+        }
+        container = this._containers[ctnOrigin];
+        if (!container) {
+          $log.warn("Ctn " + ctnOrigin + " missing: try to load it remotely");
+          promise = $http.get(ctnOrigin);
+          return promise;
+        }
+        return container;
       };
       return service;
     }
@@ -44,31 +134,61 @@
       return {
         restrict: "E",
         scope: {
-          item: "="
+          items: "="
         },
-        template: "<div>{{item.title}}</div>"
+        template: "<ul ng-repeat=\"item in items\">\n  <li>\n    <div>{{item.title}}</div>\n  </li>\n</ul>"
       };
     }
   ]);
 
   app.directive("wlContainer", [
-    "DataRetrieverService", "$compile", "$log", function(DataRetrieverService, $compile, $log) {
+    "$compile", "$log", function($compile, $log) {
       return {
         restrict: "E",
         scope: {
-          ctn: '@',
-          uri: '@'
+          uri: '@',
+          stack: '='
         },
         link: function(scope, element, attrs) {
-          var template;
-          scope.container = DataRetrieverService.retrieveOrLoadDataStructureFor(scope.uri);
-          $log.debug(scope.uri);
-          scope.notify = function() {
+          var redraw;
+          redraw = function(currentOrigin) {
+            var template;
+            $log.debug("Going to redraw ctn " + scope.uri);
+            template = "<div class=\"row\">\n  <p>Current container uri <small>" + currentOrigin + "</small></p>  \n  <wl-" + scope.container.skin + " items=\"container.items\"></wl-" + scope.container.skin + "\">\n</div>";
+            $log.debug(template);
+            element.html(template).show();
+            $compile(element.contents())(scope);
+            return true;
+          };
+          scope.$watchCollection('stack', function(newStack, oldStack) {
+            var currentOrigin;
+            $log.debug('New value for page stack...');
+            $log.debug(oldStack[scope.uri]);
+            $log.debug(newStack[scope.uri]);
+            currentOrigin = newStack[scope.uri];
+            scope.container = scope.$parent.dataFor(currentOrigin);
+            if (!scope.container) {
+              $log.warn("Content for ctn " + scope.uri + " is missing");
+              return;
+            }
+            if (scope.container.success != null) {
+              scope.container.success(function(ctn) {
+                $log.debug(angular.fromJson(ctn));
+                scope.container = ctn;
+                return redraw(currentOrigin);
+              });
+              return scope.container.error(function(response) {
+                $log.warn("There was an issue trying to load ctn " + currentOrigin + ". I restore the container");
+                scope.container = scope.$parent.dataFor(scope.uri);
+                return redraw(scope.uri);
+              });
+            } else {
+              return redraw(currentOrigin);
+            }
+          });
+          return scope.notify = function() {
             return $log.debug("Click on element");
           };
-          template = "<div class=\"row\">\n<p>Current container uri <small>{{container.origin}}</small></p>  \n<div ng-repeat=\"item in container.items\">\n  <wl-" + container.skin + " item=\"item\" ng-click=\"notify()\"></wl-" + container.skin + "\">\n</div>\n</div>";
-          element.html(template).show();
-          $compile(element.contents())(scope);
         }
       };
     }
@@ -80,7 +200,7 @@
     var injector;
     injector = angular.bootstrap(document, ["shoof.ui"]);
     return injector.invoke([
-      'EditorService', function(EditorService) {
+      'DataRetrieverService', function(DataRetrieverService) {
         return DataRetrieverService.setInPageContainers(window.containers);
       }
     ]);
--- src/coffee/app.shoof.ui.coffee
+++ src/coffee/app.shoof.ui.coffee
@@ -6,33 +6,121 @@ app.controller "ShoofCtrl", [
   "ContextManagerService"
   "DataRetrieverService"
   "$scope"
+  "$rootScope"
   "$log"
-  (ContextManagerService, DataRetrieverService, $scope, $log) ->
-    $scope.dataFor = (containerOrigin) ->
-      DataRetrieverService.retrieveOrLoadDataStructureFor(ctn)
+  (ContextManagerService, DataRetrieverService, $scope, $rootScope, $log) ->
+    
+    $scope.stack = {}    
+    $scope.context = ContextManagerService.getContext()
+
+    contextChanged = (property, value) ->
+        
+    $rootScope.$on "contextChanged", (event, property, value) ->
+      if ContextManagerService.addProperty property, value
+        ContextManagerService.rewriteStack($scope.stack)
+        
+    $rootScope.$on "containerLoaded", (event, ctnOrigin) ->
+      $log.debug "Notified about ctn #{ctnOrigin} loading"
+      $scope.stack[ctnOrigin] = ctnOrigin
+      # TODO investigate 
+      $scope.$digest()
+
+    # Retrieve container data trough a container uri / origin
+    $scope.dataFor = (ctnOrigin) ->
+      DataRetrieverService.retrieveOrLoadDataStructureFor(ctnOrigin)
+
+    # Test fn
+    $scope.test = () ->
+      $log.debug "Rewriting ctn uri"
+      $scope.stack["/data/News1.json"] = "/data/News1---foo=a.json"
+
+    # Test fn
+    $scope.submit = () ->
+      $log.debug "submit"
+      $rootScope.$broadcast "contextChanged", $scope.contextProperty, $scope.contextPropertyValue 
+
+     # Test fn
+    $scope.reset = () ->
+      $log.debug "reset"
+      ContextManagerService.resetContext
+      ContextManagerService.resetStack $scope.stack
+      $scope.contextProperty = undefined
+      $scope.contextPropertyValue = undefined
+
 ]
 
 # Represents the Context
-app.service "ContextManagerService", [->
-  service = {}
-  service.foo = ->
-    console.log "I am the context"
-    return
-
-  service
-]
-app.service "DataRetrieverService", [
-  "$log"
+app.service "ContextManagerService", [
+  "$log", 
   ($log) ->
+  
+    service =
+      _context: {}
+      # Just a fake property
+      _allowedProperties: ['foo']
+      
+    service.getContext = ->
+      @_context
+    service.addProperty = (property, value)->
+      unless property in @_allowedProperties
+        $log.warn "ContextManager does not allow property #{property}"
+        return false 
+
+      @_context[property] = value
+      true
+    service.toString = ()->
+      out = "---"
+      for property in @_allowedProperties
+        out += "#{property}=#{@_context[property]}"
+    
+      out+= ".json"
+      out
+    service.rewriteOrigin = (origin)->
+      origin.replace ".json", @toString()
+    service.rewriteStack = (stack)->
+      for id, origin of stack  
+        stack[id] = @rewriteOrigin id
+      stack
+    service.resetStack = (stack)->
+      for id, origin of stack  
+        stack[id] = id
+      stack
+    
+    service.resetContext = ()->
+      @_context = {}
+
+    service
+]
+
+app.service "DataRetrieverService", [
+  "$http",
+  "$log",
+  "$rootScope",
+  ($http, $log, $rootScope) ->
     
     service = 
       _containers: {} 
+    
     service.setInPageContainers = (containers) ->
-      $log.debug "Going to set containers #{containers}"
       @_containers = containers
+      # Loops on containers and notify them to the controller
+      for origin of @_containers
+        $log.debug "Going to notify ctn #{origin} was loaded!"
+        $rootScope.$broadcast "containerLoaded", origin
 
-    service.retrieveOrLoadDataStructureFor = (containerOrigin) ->
-      @_containers[containerOrigin]
+    service.retrieveOrLoadDataStructureFor = (ctnOrigin) ->
+      unless ctnOrigin
+        return 
+
+      container = @_containers[ctnOrigin]
+      
+      unless container
+        $log.warn "Ctn #{ctnOrigin} missing: try to load it remotely"
+              
+        promise = $http.get ctnOrigin
+        return promise
+
+      container
     
     # loop containers and find container with type = containerType
     # find origins uri for that container
@@ -48,41 +136,77 @@ app.directive "wlNews", [
     return (
       restrict: "E"
       scope:
-        item: "="
-      template: "<div>{{item.title}}</div>"
+        items: "="
+      template: """
+        <ul ng-repeat="item in items">
+          <li>
+            <div>{{item.title}}</div>
+          </li>
+        </ul>
+      """
     )
 ]
 
 # Generic container directive
 app.directive "wlContainer", [
-  "DataRetrieverService"
   "$compile"
   "$log"
-  (DataRetrieverService, $compile, $log) ->
+  ($compile, $log) ->
     return (
       restrict: "E"
       scope:
-        ctn: '@'
         uri: '@'
-
+        stack: '='
       link: (scope, element, attrs) ->
-        # Load container
-        scope.container = DataRetrieverService.retrieveOrLoadDataStructureFor(scope.uri)
-        $log.debug scope.uri
+
+        redraw = (currentOrigin)->
+          $log.debug "Going to redraw ctn #{scope.uri}"
+          template = """
+            <div class="row">
+              <p>Current container uri <small>#{currentOrigin}</small></p>  
+              <wl-#{scope.container.skin} items="container.items"></wl-#{scope.container.skin}">
+            </div>"""
+
+          $log.debug template
+          
+          element.html(template).show()
+          $compile(element.contents()) scope
+          true
+
+        # Observe stack chnages
+        scope.$watchCollection 'stack', (newStack, oldStack)->
+          
+          $log.debug 'New value for page stack...'    
+          $log.debug oldStack[scope.uri]
+          $log.debug newStack[scope.uri]
+          
+          # if newStack[scope.uri] is oldStack[scope.uri]
+          #   $log.debug "Nothing to do indeed"
+          #   return
+          currentOrigin = newStack[scope.uri]
+          # TODO $parent scope usage should be avoided
+          scope.container = scope.$parent.dataFor(currentOrigin)
+          
+          unless scope.container
+            $log.warn "Content for ctn #{scope.uri} is missing"
+            return 
+
+          if scope.container.success?
+            scope.container.success (ctn) ->
+              $log.debug angular.fromJson(ctn)
+              scope.container = ctn
+              redraw(currentOrigin)
+            scope.container.error (response) ->
+              $log.warn "There was an issue trying to load ctn #{currentOrigin}. I restore the container"
+              scope.container = scope.$parent.dataFor(scope.uri)
+              redraw(scope.uri)  
+          else
+            redraw(currentOrigin) 
+           
+
         scope.notify = ()->
           $log.debug "Click on element"
-        
-        template = """
-          <div class="row">
-          <p>Current container uri <small>{{container.origin}}</small></p>  
-          <div ng-repeat="item in container.items">
-            <wl-#{container.skin} item="item" ng-click="notify()"></wl-#{container.skin}">
-          </div>
-          </div>"""
 
-        element.html(template).show()
-        $compile(element.contents()) scope
-        return
     )
 ]
 
@@ -90,7 +214,7 @@ window.containers = {}
 
 $( document ).ready ()->
   injector = angular.bootstrap(document, ["shoof.ui"])
-  injector.invoke(['EditorService', (EditorService) ->
+  injector.invoke(['DataRetrieverService', (DataRetrieverService) ->
     DataRetrieverService.setInPageContainers window.containers
   ])
 
